34254805cd8dda7779d8989ac4eb6549a94ffa89
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2025-05-12 23:57:22 +0200

    Create Link and Node data structures

---
+++ src/structures/Link.py
@@ -0,0 +1,4 @@
+class Link:
+    def __init__(self, link_id: str, connection: tuple):
+        self.link_id = link_id
+        self.connection = connection
---
+++ src/structures/Node.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, node_id: str, longitude_latitude: tuple):
+        self.node_id = node_id
+        self.longitude_latitude = longitude_latitude
+
+    def __str__(self):
+        return f"{self.node_id} ( {self.longitude_latitude[0]} {self.longitude_latitude[1]} )"
+    
+    def _set_node_id(self, new_node_id):
+        self.node_id = new_node_id
+
+    def _get_node_id(self):
+        return self.node_id
