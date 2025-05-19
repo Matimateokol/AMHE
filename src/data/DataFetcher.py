f0e914d5b24c5707053845b563f92f7bb26b1b05
Mateusz <mateuszkolacz2019@gmail.com>
2025-05-19 02:31:09 +0200

    Implement data fetcher and fetch data

--- src/data/DataFetcher.py
+++ src/data/DataFetcher.py
@@ -1,11 +1,28 @@
 import requests
 import shutil
 
+datasets = [ 'polska', 'germany50', 'janos-us-ca']
+
+default_xml_url_path = "https://sndlib.put.poznan.pl/download/sndlib-networks-xml/"
+default_txt_url_path = "https://sndlib.put.poznan.pl/download/sndlib-networks-native/"
+
 headers = {'User-Agent': 'Mozilla'}
-urlfile = "https://sndlib.put.poznan.pl/download/sndlib-networks-xml/polska.xml"
 
-request = requests.get(urlfile, headers=headers)
-response = request.status_code
-
-with open("../../data/raw/polska.xml", 'wb') as outfile:
-    shutil.copyfileobj(response, outfile)
+def fetch_data(data_format):
+    base_url = ''
+    write_mode = 'wb'
+    if data_format == 'xml':
+        base_url = default_xml_url_path
+    elif data_format == 'txt':
+        base_url = default_txt_url_path
+    else:
+        print("Unrecognized data format - supported formats are 'xml' and 'txt' (ascii)!")
+        return
+    
+    for dataset in datasets:
+        full_url = f"{base_url}{dataset}.{data_format}"
+        response = requests.get(full_url, headers=headers, stream=True)
+        with open(f"./data/raw/{dataset}.{data_format}", write_mode) as outfile:
+            shutil.copyfileobj(response.raw, outfile)
+    
+fetch_data('txt')
